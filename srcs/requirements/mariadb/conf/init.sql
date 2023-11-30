3e932982b12d256d59e1bbbddb704817509ab228
tico <chevrier.titouan@gmail.com>
2023-11-30 13:50:34 +0000

    30/11-13:50

--- srcs/requirements/mariadb/conf/init.sql
+++ srcs/requirements/mariadb/conf/init.sql
@@ -1,10 +1,3 @@
--- init.sql
-
--- Create the database
 CREATE DATABASE IF NOT EXISTS ticodb;
-
--- Create the user
 CREATE USER 'tico45'@'%' IDENTIFIED BY 'tico45';
-
--- Grant privileges
 GRANT ALL PRIVILEGES ON ticodb.* TO 'tico45'@'%';
